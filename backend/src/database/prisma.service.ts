69142edd57fb7c4b433f2d4321e2411da4f15aa4
Mariam Mohammed <74053841+MariamMohammed-20194798@users.noreply.github.com>
2026-05-07 13:58:36 +0300

    add console on prisma.service

--- backend/src/database/prisma.service.ts
+++ backend/src/database/prisma.service.ts
@@ -4,9 +4,10 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+  implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
+    console.log("DB URL:", process.env.DATABASE_URL?.slice(0, 60));
+
     await this.$connect();
   }
 
