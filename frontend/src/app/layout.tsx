3f7cb999386d89f0c868bd029f0affa158fd6732
Mariam Mohammed <74053841+MariamMohammed-20194798@users.noreply.github.com>
2026-05-14 14:43:38 +0300

    feat: fixed smooth scrolling warning

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body className={`min-h-dvh flex flex-col`} suppressHydrationWarning={true}>
         <QueryProvider>
           <Header />
